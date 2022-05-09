0655b75303feb4e30048a60c225af3ed2f87e04b
alchonokk <galunia_suglob@mail.ru>
2022-05-09 17:08:31 +0300

    feat: add LinksEnum

---
+++ src/components/LinksEnum.tsx
@@ -0,0 +1,11 @@
+enum Links {
+  welcomePage= '/',
+  signPage='/sign',
+  profilePage="/profile",
+  editProfilePage= '/profile',
+  mainPage= '/main',
+  boardPage= '/board',
+  notFoundPage='/*'
+}
+
+export default Links
