fdcda46b19da6118fce6a88bef5032d989465424
alchonokk <galunia_suglob@mail.ru>
2022-05-10 22:34:07 +0300

    refactor: delete layout

--- src/components/RoutesWrapper.tsx
+++ src/components/RoutesWrapper.tsx
@@ -1,29 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
-
-import LayoutApp from './LayoutApp';
 import { BoardPage } from '../pages/BoardPage';
 import { MainPage } from '../pages/MainPage';
 import { NotFoundPage } from '../pages/NotfoundPage';
 import { WelcomePage } from '../pages/WelcomePage';
 import { EditProfilePage } from '../pages/EditProfilePage';
-import { SignPage } from '../pages/LoginPage';
+import { SignPage } from '../pages/SignPage';
 import Links from './LinksEnum';
 
-function RoutesWrapper() {
+const RoutesWrapper=()=> {
   return (
-    <>
-      <LayoutApp></LayoutApp>
-      <Routes>
-          <Route path={Links.welcomePage} element={<WelcomePage />} />
-          <Route path={Links.signPage} element={<SignPage />} />
-          <Route path={Links.profilePage} element={<EditProfilePage />} />
-          <Route path={Links.mainPage} element={<MainPage />} />
-          <Route path={Links.boardPage} element={<BoardPage />} />
-          <Route path={Links.notFoundPage} element={<NotFoundPage />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path={Links.welcomePage} element={<WelcomePage />} />
+      <Route path={Links.mainPage} element={<MainPage />} />
+      <Route path={Links.signPage} element={<SignPage />} />
+      <Route path={Links.profilePage} element={<EditProfilePage />} />
+      <Route path={Links.boardPage} element={<BoardPage />} />
+      <Route path={Links.notFoundPage} element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
-export default RoutesWrapper;
+export {RoutesWrapper};
 
