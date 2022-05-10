f97f8998b5d5c52259f9bf7af7af9b64a6a22986
alchonokk <galunia_suglob@mail.ru>
2022-05-10 22:32:44 +0300

    refactor: delete hash

--- src/components/LinksEnum.tsx
+++ src/components/LinksEnum.tsx
@@ -1,11 +1,10 @@
 enum Links {
   welcomePage= '/',
-  signPage='/sign',
-  profilePage="/profile",
-  editProfilePage= '/profile',
-  mainPage= '/main',
-  boardPage= '/board',
-  notFoundPage='/*'
+  signPage='sign',
+  profilePage="profile",
+  mainPage= 'main',
+  boardPage= 'board',
+  notFoundPage='*'
 }
 
 export default Links
