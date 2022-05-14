e92d5fc57369a106b044fdab48353b9855b38cab
Ilya Dzyuin <ilua.dzuin@gmail.com>
2022-05-14 12:33:00 +0400

    fix: fix code with eslint

--- src/API/files.tsx
+++ src/API/files.tsx
@@ -1,16 +1,16 @@
-import { baseURL, requestAPI, Methods, IUserData } from "./dependencies";
+import { baseURL, requestAPI, Methods, IUserData } from './dependencies';
 
-interface IFile{
+interface IFile {
   taskId: string;
   file: BinaryData;
 }
 
 const baseFileUrl = `${baseURL}/file`;
 
-export async function downloadFile(token: string, data: IFile){
+export async function downloadFile(token: string, data: IFile) {
   //some logic
 }
 
-export async function uploadFile(token: string, taskId: string, fileName: string){
+export async function uploadFile(token: string, taskId: string, fileName: string) {
   //some logic
 }
--- src/API/users.tsx
+++ src/API/users.tsx
@@ -1,44 +1,44 @@
-import { baseURL, requestAPI, Methods, IUserData } from "./dependencies";
+import { baseURL, requestAPI, Methods, IUserData } from './dependencies';
 
 const usersBaseURL = `${baseURL}users/`;
 
-export async function getUser(token: string, id?: string){
-  const URL = `${usersBaseURL}${id? id: ''}`
+export async function getUser(token: string, id?: string) {
+  const URL = `${usersBaseURL}${id ? id : ''}`;
   const options = {
-    'method': Methods.get,
+    method: Methods.get,
     headers: {
-      'Authorization': `Bearer ${token}`,
-      'Accept': 'application/json',
-    }
-  } as Partial<RequestInit>
-  const data = await requestAPI<IUserData | IUserData[]>({URL, options});
-  return data;
-}
-
-export async function deleteUser(token: string, id: string){
-  const URL = `${usersBaseURL}${id}`;
-  const options = {
-    'method': Methods.delete,
-    headers: {
-      'Authorization': `Bearer ${token}`,
-      'Accept': 'application/json',
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json'
     }
   } as Partial<RequestInit>;
-  const data = await requestAPI<IUserData>({URL, options});
+  const data = await requestAPI<IUserData | IUserData[]>({ URL, options });
   return data;
 }
 
-export async function updateUser(token: string, id: string, body: IUserData){
+export async function deleteUser(token: string, id: string) {
   const URL = `${usersBaseURL}${id}`;
   const options = {
-    'method': Methods.put,
+    method: Methods.delete,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json'
+    }
+  } as Partial<RequestInit>;
+  const data = await requestAPI<IUserData>({ URL, options });
+  return data;
+}
+
+export async function updateUser(token: string, id: string, body: IUserData) {
+  const URL = `${usersBaseURL}${id}`;
+  const options = {
+    method: Methods.put,
     headers: {
       'Authorization': `Bearer ${token}`,
       'Accept': 'application/json',
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(body),
-  } as Partial<RequestInit>
-  const data = await requestAPI<IUserData>({URL, options});
+    body: JSON.stringify(body)
+  } as Partial<RequestInit>;
+  const data = await requestAPI<IUserData>({ URL, options });
   return data;
 }
