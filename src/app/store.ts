265e134732c6adc8a077d856ec153653784dbdd7
alchonokk <galunia_suglob@mail.ru>
2022-05-14 21:03:22 +0300

    feat: add redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,37 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import authorizationOfUserReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
+import authorizationOfUserReducer from '../features/authorization/authorizationSlice';
+import { persistStore, persistReducer, 
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+ } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const rootReducer = combineReducers({
+  authorizationOfUser: authorizationOfUserReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    authorizationOfUser: authorizationOfUserReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
