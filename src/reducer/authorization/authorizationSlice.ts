37c9f1b588f091893a52eedae9294fece96580dc
alchonokk <galunia_suglob@mail.ru>
2022-05-16 00:21:23 +0300

    feat: add status for SignIn and SignUp

--- src/reducer/authorization/authorizationSlice.ts
+++ src/reducer/authorization/authorizationSlice.ts
@@ -5,7 +5,8 @@ import {signIn, signUp} from '../../API/authorization';
 export interface userAuthorizationState {
   dataOfSignUp: {id: string, name: string, login: string};
   dataOfSignIn: {token:string};
-  status: 'fulfilled' | 'loading' | 'rejected';
+  statusSignUp: string;
+  statusSignIn: string;
   errorOfSignIn: string;
   errorOfSignUp: string;
 }
@@ -13,7 +14,8 @@ export interface userAuthorizationState {
 const initialState: userAuthorizationState = {
   dataOfSignUp: {id: '', name: '', login: ''},
   dataOfSignIn: {token:''},
-  status: 'fulfilled',
+  statusSignUp: '',
+  statusSignIn: '',
   errorOfSignIn: 'User was not founded!',
   errorOfSignUp: 'User login already exists!',
 };
@@ -30,10 +32,10 @@ export const userAuthorizationSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(createAsyncSignUp.pending, (state) => {
-        state.status = 'loading';
+        state.statusSignUp = 'loading';
       })
       .addCase(createAsyncSignUp.fulfilled, (state, action:PayloadAction<any>) => {
-        state.status = 'fulfilled';
+        state.statusSignUp = 'fulfilled';
         if (action.payload){
           state.dataOfSignUp = action.payload;
         }
@@ -42,15 +44,15 @@ export const userAuthorizationSlice = createSlice({
         if (action.error.message){
           state.errorOfSignUp = action.error.message;
         }
-        state.status = 'rejected';
+        state.statusSignUp = 'rejected';
       });
 
     builder
       .addCase(createAsyncSignIn.pending, (state) => {
-        state.status = 'loading';
+        state.statusSignIn = 'loading';
       })
       .addCase(createAsyncSignIn.fulfilled, (state, action:PayloadAction<any>) => {
-        state.status = 'fulfilled';
+        state.statusSignIn = 'fulfilled';
         if (action.payload){
           state.dataOfSignIn = action.payload;
         }
@@ -59,14 +61,15 @@ export const userAuthorizationSlice = createSlice({
         if (action.error.message){
           state.errorOfSignIn = action.error.message;
         }
-        state.status = 'rejected';
+        state.statusSignIn = 'rejected';
       });
   },
 });
 
 export const valueApiSignUp = (state: RootState) => state.userAuthorization.dataOfSignUp;
 export const tokenFromApiSignIn = (state: RootState) => state.userAuthorization.dataOfSignIn.token;
-export const statusFromApiSignIn = (state: RootState) => state.userAuthorization.status;
+export const statusFromApiSignIn = (state: RootState) => state.userAuthorization.statusSignIn;
+export const statusFromApiSignUp = (state: RootState) => state.userAuthorization.statusSignUp;
 export const errorFromApiSignIn = (state: RootState) => state.userAuthorization.errorOfSignIn;
 export const errorFromApiSignUp = (state: RootState) => state.userAuthorization.errorOfSignUp;
 
