f73e4f8322a9aac7d2662ba6f3ae2c0adc9f6bc5
alchonokk <galunia_suglob@mail.ru>
2022-05-15 22:49:17 +0300

    feat: add style from i18n

--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import './i18n';
+import { Typography } from 'antd';
+
+const { Title } = Typography;
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Suspense fallback={<h1>Загрузка...</h1>}>
+    <Suspense fallback={<Title level={5}>Загрузка...</Title>}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <BrowserRouter>
