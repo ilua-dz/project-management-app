862f86b686d4118d0b808dcf23331c83c2b07533
alchonokk <galunia_suglob@mail.ru>
2022-05-16 22:13:45 +0300

    feat: add isActionFulfilled

---
+++ src/app/actionHelper.tsx
@@ -0,0 +1,9 @@
+import { PayloadAction, SerializedError } from "@reduxjs/toolkit";
+import { Token, SignInData, SignUpData } from "../API/authorization";
+import { IUserData } from "../API/dependencies";
+
+function isActionFulfilled( action: PayloadAction<void | Error | Token | undefined, string, { arg: SignInData; requestId: string; requestStatus: "fulfilled"; }, never> | PayloadAction<unknown, string, { arg: SignInData; requestId: string; requestStatus: "rejected"; aborted: boolean; condition: boolean; } & ({ rejectedWithValue: true; } | ({ rejectedWithValue: false; } & {})), SerializedError> | PayloadAction<void | IUserData | Error | undefined, string, { arg: SignUpData; requestId: string; requestStatus: "fulfilled"; }, never> ) {
+  return action.meta.requestStatus=== 'fulfilled';
+}
+
+export default isActionFulfilled;
