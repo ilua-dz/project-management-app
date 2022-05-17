2441d5e1baf75a749793614a004c5ba49fdd8b8b
Ilya Dzyuin <ilua.dzuin@gmail.com>
2022-05-18 00:57:00 +0400

    feat: add main page layout

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,64 @@
+import styled, { CSSProperties } from 'styled-components';
+import { List, Card } from 'antd';
+import { Typography } from 'antd';
+import { AppstoreOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import PageTitle from '../../components/styled/PageTitle';
+
+const { Title } = Typography;
+
+const fakeData = [
+  {
+    title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, explicabo!'
+  }
+];
+
 function MainPage() {
   return (
-    <>
-      <div>
-        <h1>Main page</h1>
-      </div>
-    </>
+    <Container>
+      <PageTitle textLink="titles.main-page" icon={<AppstoreOutlined />} />
+      <List
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 1,
+          md: 2,
+          lg: 3,
+          xl: 3,
+          xxl: 3
+        }}
+        dataSource={fakeData}
+        renderItem={(item) => (
+          <List.Item>
+            <Card
+              bodyStyle={cardBodyStyle}
+              hoverable
+              actions={[<EditOutlined key="edit" />, <DeleteOutlined key="delete" />]}>
+              <StyledTitle level={4}>{item.title}</StyledTitle>
+            </Card>
+          </List.Item>
+        )}
+      />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  & .ant-row {
+    margin: 0 !important;
+    justify-content: center;
+  }
+`;
+
+const StyledTitle = styled(Title)`
+  text-align: center;
+  margin: 0 !important;
+`;
+
+const cardBodyStyle: CSSProperties = {
+  minHeight: '9rem',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+};
 
 export default MainPage;
