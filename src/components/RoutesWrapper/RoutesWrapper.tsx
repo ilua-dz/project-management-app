fd4f4767c5a510b43e3d0fad5ffc0f18e7ae09d6
alchonokk <galunia_suglob@mail.ru>
2022-05-19 22:00:32 +0300

    feat: add RequireAuthorization in route

--- src/components/RoutesWrapper/RoutesWrapper.tsx
+++ src/components/RoutesWrapper/RoutesWrapper.tsx
@@ -8,14 +8,29 @@ import SignInPage from '../../pages/SignInPage';
 import SignUpPage from '../../pages/SignUpPage';
 import Links from '../../enumerations/LinksEnum';
 import LayoutApp from '../LayoutApp';
+import RequireAuthorization from './RequireAuth';
 
 function RoutesWrapper() {
   return (
     <Routes>
       <Route path={Links.welcomePage} element={<LayoutApp />}>
         <Route index element={<WelcomePage />} />
-        <Route path={Links.signInPage} element={<SignInPage />} />
-        <Route path={Links.signUpPage} element={<SignUpPage />} />
+        <Route
+          path={Links.signInPage}
+          element={
+            <RequireAuthorization>
+              <SignInPage />
+            </RequireAuthorization>
+          }
+        />
+        <Route
+          path={Links.signUpPage}
+          element={
+            <RequireAuthorization>
+              <SignUpPage />
+            </RequireAuthorization>
+          }
+        />
         <Route path={Links.profilePage} element={<EditProfilePage />} />
         <Route path={Links.mainPage} element={<MainPage />} />
         <Route path={Links.boardPage} element={<BoardPage />} />
