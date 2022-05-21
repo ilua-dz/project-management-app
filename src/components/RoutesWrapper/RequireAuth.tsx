5f6c647a1e30975a91405b524429b2e2995d39a7
alchonokk <galunia_suglob@mail.ru>
2022-05-21 14:12:43 +0300

    fix: change logik RequireAuthorization

--- src/components/RoutesWrapper/RequireAuth.tsx
+++ src/components/RoutesWrapper/RequireAuth.tsx
@@ -1,33 +1,25 @@
 import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
 import Links from '../../enumerations/LinksEnum';
-import {
-  getApiSignInToken,
-  getApiSignUpData
-} from '../../reducer/authorization/authorizationSlice';
+import { getApiSignInToken } from '../../reducer/authorization/authorizationSlice';
 
 type Props = {
   children: JSX.Element;
+  needToBeUnauthorized: boolean;
 };
 
-function RequireAuthorization({ children }: Props) {
+function RequireAuthorization({ children, needToBeUnauthorized }: Props) {
   const token = useAppSelector(getApiSignInToken);
 
-  // Первый цикл для того, чтобы бы после удачной авторизации был автоматический переход на signInPage,
-  // если убрать этот цикл, то после авторизации юзер видит сообщение, что авторизован(Регистрация завершена. Войдите под своим именем),
-  // но сам должен перейти на signInPage. Как лучше? В ниже приведенном варианте юзер после авторизации уже не может перейти на signUpPage
-
-  const dataSignUp = useAppSelector(getApiSignUpData);
-  const location = useLocation();
-
-  if (location.pathname === Links.signUpPage && dataSignUp.id) {
-    return <Navigate to={Links.signInPage} replace />;
+  if (!token && !needToBeUnauthorized) {
+    return <Navigate to={Links.welcomePage} replace />;
   }
 
-  if (token) {
+  if (token && needToBeUnauthorized) {
     return <Navigate to={Links.mainPage} replace />;
   }
+
   return children;
 }
 
